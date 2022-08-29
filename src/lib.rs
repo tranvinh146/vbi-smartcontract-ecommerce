0ee4ff5de696cc73a845b03f095efe7cc02eddb6
tranvinh146 <vinh.tt@geekup.vn>
2022-08-29 21:31:34 +0700

    fix cancal_order

--- src/lib.rs
+++ src/lib.rs
@@ -2,8 +2,8 @@ use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
 use near_sdk::collections::LookupMap;
 use near_sdk::json_types::U128;
 use near_sdk::{
-    env, ext_contract, near_bindgen, AccountId, BorshStorageKey, PanicOnDefault, Promise,
-    PromiseOrValue,
+    env, ext_contract, near_bindgen, AccountId, BorshStorageKey, Gas, PanicOnDefault, Promise,
+    PromiseOrValue, PromiseResult,
 };
 
 mod order;
@@ -11,7 +11,7 @@ use order::*;
 
 #[derive(BorshDeserialize, BorshSerialize, PanicOnDefault)]
 #[near_bindgen]
-struct EcommerceContract {
+pub struct EcommerceContract {
     pub owner_id: AccountId,
     pub orders: LookupMap<OrderId, Order>,
 }
@@ -21,9 +21,11 @@ enum StorageKey {
     OrderKey,
 }
 
-// enum Error {
-//     ERROR_NOT_FOUND_ORDER_ID,
-// }
+pub const ERROR_DEPOSIT_NOT_ENOUGH: &str = "ERROR_DEPOSIT_NOT_ENOUGH";
+pub const ERROR_NOT_FOUND_ORDER_ID: &str = "ERROR_NOT_FOUND_ORDER_ID";
+pub const ERROR_TOO_MANY_RESULTS: &str = "ERROR_TOO_MANY_RESULTS";
+
+pub const SET_REFUND_GAS: Gas = Gas(10_000_000_000_000);
 
 #[near_bindgen]
 impl EcommerceContract {
@@ -39,7 +41,8 @@ impl EcommerceContract {
     pub fn pay_order(&mut self, order_id: OrderId, order_amount: U128) -> PromiseOrValue<U128> {
         assert!(
             env::attached_deposit() >= order_amount.0,
-            "ERROR_DEPOSIT_NOT_ENOUGH"
+            "{}",
+            ERROR_DEPOSIT_NOT_ENOUGH
         );
 
         let order = Order {
@@ -64,30 +67,56 @@ impl EcommerceContract {
     }
 
     pub fn get_order(&self, order_id: OrderId) -> Order {
-        self.orders
-            .get(&order_id)
-            .expect("ERROR_NOT_FOUND_ORDER_ID")
+        self.orders.get(&order_id).expect(ERROR_NOT_FOUND_ORDER_ID)
     }
 
-    pub fn cancel_order(&mut self, order_id: OrderId) {
-        let mut order = self.get_order(order_id);
-        let payer_id = order.payer_id;
-        assert_eq!(payer_id, env::predecessor_account_id());
-        Promise::new(env::predecessor_account_id()).transfer(env::attached_deposit());
-        // .then(ext_self::set_refunded());
-        self.set_refunded(order.order_id);
-    }
+    pub fn cancel_order(&self, order_id: OrderId) -> PromiseOrValue<U128> {
+        assert_eq!(env::predecessor_account_id(), self.owner_id);
 
-    fn set_refunded(&mut self, order_id: OrderId) {
-        let mut order = self.get_order(order_id);
-        order.is_refund = true;
+        let order = self.get_order(order_id.clone());
+        assert!(order.is_completed && !order.is_refund);
+
+        if order.amount > 0 {
+            let promise = Promise::new(order.payer_id).transfer(order.amount).then(
+                ext_self::ext(env::current_account_id())
+                    .with_attached_deposit(0)
+                    .with_static_gas(SET_REFUND_GAS)
+                    .set_is_refunded(order_id),
+            );
+            PromiseOrValue::Promise(promise)
+        } else {
+            PromiseOrValue::Value(U128(0))
+        }
     }
 }
 
-// #[ext_contract(ext_self)]
-// pub trait ExtSelf {
-//     fn set_refunded(&mut self, order_id: OrderId);
-// }
+#[ext_contract(ext_self)]
+pub trait ExtSelf {
+    fn set_is_refunded(&mut self, order_id: OrderId) -> PromiseOrValue<U128>;
+}
+
+#[near_bindgen]
+impl ExtSelf for EcommerceContract {
+    #[private]
+    fn set_is_refunded(&mut self, order_id: OrderId) -> PromiseOrValue<U128> {
+        assert_eq!(
+            env::promise_results_count(),
+            1,
+            "{}",
+            ERROR_TOO_MANY_RESULTS
+        );
+        match env::promise_result(0) {
+            PromiseResult::Successful(_) => {
+                let mut order = self.orders.get(&order_id).expect(ERROR_NOT_FOUND_ORDER_ID);
+                order.is_refund = true;
+                self.orders.insert(&order_id, &order);
+
+                PromiseOrValue::Value(U128(order.amount))
+            }
+            _ => PromiseOrValue::Value(U128(0)),
+        }
+    }
+}
 
 #[cfg(test)]
 mod tests {
